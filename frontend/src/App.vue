e546b8030fd576d703f5b281cc98f49c47c71b7a
renzo <rensoliscastillo@gmail.com>
2020-05-20 00:29:04 -0500

    setup backend and first setup for frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div id="nav" class="font-light">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
